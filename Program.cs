9da6f47431809e83478e2ce83d69ba254d346f2d
arzualkann <arzualkann@outlook.com>
2024-02-19 19:21:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using kodlama.io_odev3_oyun;
+
+GamerManager gamerManager = new GamerManager(new UserValidationManager());
+gamerManager.Add(new Gamer { Id = 1, BirthYear = 1998, FirstName = "ARZU", LastName = "ALKAN", IdentityNumber = 12562 });
---
+++ UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodlama.io_odev3_oyun
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear==1985 && gamer.FirstName=="ENGİN"
+                &&gamer.LastName=="DEMİROĞ"&&gamer.Id==12562)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
